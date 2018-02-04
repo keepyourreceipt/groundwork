dbdb7e1ac3cfdc94fd4018e546b344d47ec8b8ad
Michael <michaelkpeterman@gmail.com>
2018-02-04 16:48:15 -0500

    Add WC sidebar override. Add body class() to header

--- app/functions.php
+++ app/functions.php
@@ -3,6 +3,8 @@
 WordPress feature theme support
 ***********************************************/
 add_theme_support('menus');
+add_theme_support('widgets');
+add_theme_support('woocommerce');
 
 function theme_custom_logo() {
     $defaults = array(
@@ -14,6 +16,23 @@ function theme_custom_logo() {
     add_theme_support( 'custom-logo', $defaults );
 }
 add_action( 'after_setup_theme', 'theme_custom_logo' );
+
+/***********************************************
+Add widget areas / theme sidebars
+***********************************************/
+add_action( 'widgets_init', 'groundwork_widgets_init' );
+
+function groundwork_widgets_init() {
+  register_sidebar( array(
+      'name' => __( 'Shop Sidebar', 'groundwork' ),
+      'id' => 'sidebar-1',
+      'description' => __( 'Widgets in this area will be shown on the Shop page', 'groundwork' ),
+      'before_widget' => '<li id="%1$s" class="widget %2$s">',
+      'after_widget'  => '</li>',
+      'before_title'  => '<h2 class="widgettitle">',
+      'after_title'   => '</h2>',
+  ) );
+}
 
 /***********************************************
 Add custom menu display locations
@@ -28,7 +47,7 @@ add_action( 'init', 'register_menu_locations' );
 Enqueue theme scripts and stylesheets
 ***********************************************/
 function enqueue_theme_styles() {
-    wp_enqueue_style( 'default', get_stylesheet_uri() );
-    wp_enqueue_style( 'theme', get_template_directory_uri() . '/css/style.css');
+    wp_enqueue_style( 'default-styles', get_stylesheet_uri() );
+    wp_enqueue_style( 'theme-styles', get_template_directory_uri() . '/css/style.css');
 }
 add_action( 'wp_enqueue_scripts', 'enqueue_theme_styles' );

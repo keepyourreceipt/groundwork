1392c40d83ad5190c313f8098184df827227767d
Michael <michaelkpeterman@gmail.com>
2018-02-06 23:13:06 -0500

    Add fancy single product select elements

--- app/js/scripts.js
+++ app/js/scripts.js
@@ -1,3 +1,28 @@
-jQuery(document).ready(function( $ ) {
 
+jQuery(document).ready(function( $ ) {
+  function themeInit() {
+    assignProductColorRadios();
+    fancyProductQuantityField();
+  }
+
+  function assignProductColorRadios() {
+    var $colorVariationsTable = $('table.variations tr.attribute-color');
+    if ( $colorVariationsTable.length ) {
+      $colorVariationsTable.find('input[type="radio"]').each(function() {
+        var color = $(this).attr('value');
+        var radioLabel = $(this).siblings('label');
+        radioLabel.css('background-color', color);
+      });
+    }
+  }
+
+  function fancyProductQuantityField() {
+    var $quantityContainer = $('.single-product input.qty');
+    if ( $quantityContainer.length ) {
+      $('<div class="quantity-decrease"></div>').insertBefore( $quantityContainer );
+      $('<div class="quantity-increase"></div>').insertAfter( $quantityContainer );
+    }
+  }
+
+  themeInit();
 });

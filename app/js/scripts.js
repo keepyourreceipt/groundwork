e56f3a372cc0e9560860f1c06ae325174386e72b
Michael <michaelkpeterman@gmail.com>
2018-02-10 00:03:50 -0500

    Update single product qty field increment and decrement

--- app/js/scripts.js
+++ app/js/scripts.js
@@ -21,6 +21,32 @@ jQuery(document).ready(function( $ ) {
     if ( $quantityContainer.length ) {
       $('<div class="quantity-decrease"></div>').insertBefore( $quantityContainer );
       $('<div class="quantity-increase"></div>').insertAfter( $quantityContainer );
+
+      // Handle qty increment
+      $('.quantity-increase').on('click', function() {
+        var $qtyField = $(this).siblings('input[type="number"]');
+        var updatedQtyFieldValue = parseInt($qtyField.attr('value')) + 1;
+
+        // Increase qty value by one
+        $qtyField.attr('value', updatedQtyFieldValue );
+      });
+
+      // Handle qty decrement
+      $('.quantity-decrease').on('click', function() {
+        var $qtyField = $(this).siblings('input[type="number"]');
+        var qtyFieldValue = parseInt($qtyField.attr('value'));
+
+        var updatedQtyFieldValue;
+        if ( qtyFieldValue > 1 ) {
+          updatedQtyFieldValue = qtyFieldValue - 1;
+        } else {
+          // prevent user from ordering 0 qty of items
+          return;
+        }
+
+        // If number greater than 1, decrement qty value
+        $qtyField.attr('value', updatedQtyFieldValue );
+      });
     }
   }
 

5636439a04664f5bbd0caf91bbdd77ec352764fc
Michael <michaelkpeterman@gmail.com>
2018-02-25 17:31:29 -0500

    Restructure woocommerce checkout form

--- app/inc/woocommerce/checkout-hooks.php
+++ app/inc/woocommerce/checkout-hooks.php
@@ -25,3 +25,12 @@ function action_woocommerce_after_checkout_form() {
   echo "</div>"; // end container
 };
 add_action( 'woocommerce_after_checkout_form', 'action_woocommerce_after_checkout_form', 10, 2 );
+
+// Remove company name field from checkout page form
+add_filter( 'woocommerce_checkout_fields' , 'custom_override_checkout_fields' );
+
+function custom_override_checkout_fields( $fields ) {
+  unset($fields['billing']['billing_company']);
+
+  return $fields;
+}

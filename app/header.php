395da549acbd1048ffe20072380a2d0c2fd6a742
Michael <michaelkpeterman@gmail.com>
2018-02-28 23:14:13 -0500

    Add hours and phone fields. Add page.php template file

--- app/header.php
+++ app/header.php
@@ -15,6 +15,27 @@
     require_once get_template_directory() . '/inc/elementor/global-color-support.php';
   ?>
 
+  <!-- Begin toolbar -->
+  <div class="toolbar">
+    <div class="container">
+      <div class="content">        
+        <div class="hours">
+          <?php 
+            // Set default timezone to US eastern
+            date_default_timezone_set('America/New_York');
+            $hours = get_theme_mod( strtolower( date( 'l' ) ) . '_hours' ); 
+          ?>
+          <span>Hours: <?php echo $hours; ?></span>
+        </div>
+        <?php if ( get_theme_mod( 'phone_number') ) { ?>
+          <div class="phone">
+            <span><?php echo get_theme_mod( 'phone_number'); ?></span>
+          </div>
+        <?php } ?>
+      </div>
+    </div>
+  </div>
+
   <!-- Begin main menu -->
   <?php if ( has_nav_menu( 'main_menu' ) || has_custom_logo() ) { ?>
     <div class="main-menu">
--- app/inc/kirki/custom-fields.php
+++ app/inc/kirki/custom-fields.php
@@ -53,4 +53,101 @@ if(class_exists('Kirki')) {
     'default'  => esc_attr__( '', 'groundwork' ),
     'priority' => 10,
   ) );
+
+  /***********************************************************
+  * Store hours fields
+  ************************************************************/
+  Kirki::add_section( 'store_hours', array(
+    'title'          => __( 'Store Hours' ),
+    'description'    => __( 'Enter the store hours you would like to display on the site', 'groundwork' ),
+    'panel'          => '', // Not typically needed.
+    'priority'       => 20,
+    'capability'     => 'edit_theme_options',
+    'theme_supports' => '', // Rarely needed.
+) );
+
+Kirki::add_field( 'groundwork_kirki_config', array(
+  'type'     => 'text',
+  'settings' => 'monday_hours',
+  'label'    => __( 'Monday', 'groundwork' ),
+  'section'  => 'store_hours',
+  'default'  => esc_attr__( '', 'groundwork' ),
+  'priority' => 10,
+) );
+
+Kirki::add_field( 'groundwork_kirki_config', array(
+  'type'     => 'text',
+  'settings' => 'tuesday_hours',
+  'label'    => __( 'Tuesday', 'groundwork' ),
+  'section'  => 'store_hours',
+  'default'  => esc_attr__( '', 'groundwork' ),
+  'priority' => 10,
+) );
+
+Kirki::add_field( 'groundwork_kirki_config', array(
+  'type'     => 'text',
+  'settings' => 'wednesday_hours',
+  'label'    => __( 'Wednesday', 'groundwork' ),
+  'section'  => 'store_hours',
+  'default'  => esc_attr__( '', 'groundwork' ),
+  'priority' => 10,
+) );
+
+Kirki::add_field( 'groundwork_kirki_config', array(
+  'type'     => 'text',
+  'settings' => 'thursday_hours',
+  'label'    => __( 'Thursday', 'groundwork' ),
+  'section'  => 'store_hours',
+  'default'  => esc_attr__( '', 'groundwork' ),
+  'priority' => 10,
+) );
+
+Kirki::add_field( 'groundwork_kirki_config', array(
+  'type'     => 'text',
+  'settings' => 'friday_hours',
+  'label'    => __( 'Friday', 'groundwork' ),
+  'section'  => 'store_hours',
+  'default'  => esc_attr__( '', 'groundwork' ),
+  'priority' => 10,
+) );
+
+Kirki::add_field( 'groundwork_kirki_config', array(
+  'type'     => 'text',
+  'settings' => 'saturday_hours',
+  'label'    => __( 'Saturday', 'groundwork' ),
+  'section'  => 'store_hours',
+  'default'  => esc_attr__( '', 'groundwork' ),
+  'priority' => 10,
+) );
+
+Kirki::add_field( 'groundwork_kirki_config', array(
+  'type'     => 'text',
+  'settings' => 'sunday_hours',
+  'label'    => __( 'Sunday', 'groundwork' ),
+  'section'  => 'store_hours',
+  'default'  => esc_attr__( '', 'groundwork' ),
+  'priority' => 10,
+) );
+
+
+/***********************************************************
+  * Business info fields
+  ************************************************************/
+  Kirki::add_section( 'business_info', array(
+    'title'          => __( 'Business Info' ),
+    'description'    => __( 'Basic business info', 'groundwork' ),
+    'panel'          => '', // Not typically needed.
+    'priority'       => 20,
+    'capability'     => 'edit_theme_options',
+    'theme_supports' => '', // Rarely needed.
+) );
+
+Kirki::add_field( 'groundwork_kirki_config', array(
+  'type'     => 'text',
+  'settings' => 'phone_number',
+  'label'    => __( 'Phone number', 'groundwork' ),
+  'section'  => 'business_info',
+  'default'  => esc_attr__( '', 'groundwork' ),
+  'priority' => 10,
+) );
 }
